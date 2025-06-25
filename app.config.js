2c642a611848f6a24892d1864436edc3f9f950bb
NosytLabs <Tysonbarrycoughlan@gmail.com>
2025-06-25 01:14:53 -0300

    Add Expo app configuration

---
+++ app.config.js
@@ -0,0 +1,43 @@
+module.exports = {
+  expo: {
+    runtimeVersion: "1.0.0",
+    extra: {
+      eas: {
+        projectId: "2915938a-b44d-41d0-8f6a-2480c45d421f"
+      }
+    },
+    name: 'RealityCheck',
+    slug: 'realitycheck-2025',
+    version: '2.0.0',
+    owner: 'nosytlabs',
+    scheme: 'realitycheck',
+    platforms: ['ios', 'android', 'web'],
+    orientation: 'portrait',
+    userInterfaceStyle: 'automatic',
+    splash: {
+      resizeMode: 'contain',
+      backgroundColor: '#ffffff'
+    },
+    assetBundlePatterns: ['**/*'],
+    ios: {
+      supportsTablet: true,
+      bundleIdentifier: 'com.realitycheck.app'
+    },
+    android: {
+      package: 'com.realitycheck.app'
+    },
+    web: {
+      bundler: 'metro'
+    },
+    plugins: [
+      "expo-dev-client",
+      [
+        "expo-image-picker",
+        {
+          photosPermission: "The app accesses your photos to let you share them.",
+          cameraPermission: "The app accesses your camera to let you take photos."
+        }
+      ]
+    ]
+  }
+};
