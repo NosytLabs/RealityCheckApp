ddef6157de54c5d66e903d54674a4a136aba26b3
NosytLabs <Tysonbarrycoughlan@gmail.com>
2025-06-25 01:27:04 -0300

    Add Prettier configuration

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  bracketSpacing: true,
+  arrowParens: 'avoid',
+  endOfLine: 'auto',
+  jsxSingleQuote: false,
+  jsxBracketSameLine: false,
+  quoteProps: 'as-needed',
+};
