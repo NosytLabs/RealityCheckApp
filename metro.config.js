06ec46376b46e5b72a210347a9c4491ee4e1a9d5
NosytLabs <Tysonbarrycoughlan@gmail.com>
2025-06-28 22:48:31 -0300

    Fix expo-linking dependency and SVG web compatibility

--- metro.config.js
+++ metro.config.js
@@ -2,4 +2,14 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
+// Add resolver configuration for web compatibility
+config.resolver = {
+  ...config.resolver,
+  alias: {
+    ...config.resolver.alias,
+    'react-native-svg': 'react-native-svg/lib/module/index.web.js',
+  },
+  platforms: ['ios', 'android', 'native', 'web'],
+};
+
 module.exports = config;
